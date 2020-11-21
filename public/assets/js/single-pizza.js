bb2a9cf2866fa0fb062634a62c7b22fc7c61d686
natswatch <nathaliec@me.com>
2020-11-20 22:34:57 -0800

    reply feature

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -108,6 +108,28 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+  
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    response.json();
+  })
+  .then(commentResponse => {
+    console.log(commentResponse);
+    location.reload();
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
 
 function handleNewReplySubmit(event) {
@@ -127,6 +149,28 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    response.json();
+  })
+  .then(replyResponse => {
+    console.log(replyResponse);
+    location.reload();
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
 
 $backBtn.addEventListener('click', function() {
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment,
+    addReply, 
+    removeComment,
+    removeReply
+    } = require('../../controllers/comment-controller');
+
 router
     .route('/:pizzaId')
     .post(addComment);
 
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment);
 
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 module.exports = router;
